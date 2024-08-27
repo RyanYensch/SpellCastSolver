3285c60350afbb17a33fdebc6a8282ef53244eee
Ryan Yensch <z5585114@student.unsw.edu.au>
2024-08-27 18:23:20 +1000

    made it fill the board

--- class.py
+++ class.py
@@ -1,4 +1,30 @@
 class App:
+  
   def __init__(self):
     self.board = []
     self.wordList = []
+
+  def fillBoard(self):
+    self.board.clear()
+
+    print("Enter Board")
+    for i in range (0, 5):
+      line = input("Line {}: ".format(i+1))
+      row = []
+      for j in range(0,5):
+        row.append(line[j])
+      self.board.append(row)
+    return self 
+
+   
+
+  
+    
+
+
+
+
+
+if __name__ == "__main__":
+  Game = App()
+  Game.fillBoard()
