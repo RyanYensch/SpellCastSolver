fd45293bb1e75c3fe8404d6627ee865d7522adb4
Ryan Yensch <z5585114@student.unsw.edu.au>
2024-08-27 18:43:14 +1000

    creates a list of words that cant be made witht he letters

--- class.py
+++ class.py
@@ -1,8 +1,15 @@
 class App:
-  
+
   def __init__(self):
     self.board = []
     self.wordList = []
+    self.validWordsForBoard = []
+
+
+    with open("words.txt", 'r') as file:
+      for line in file:
+        word = line.strip()
+        self.wordList.append(word)
 
   def fillBoard(self):
     self.board.clear()
@@ -14,10 +21,25 @@ class App:
       for j in range(0,5):
         row.append(line[j])
       self.board.append(row)
-    return self 
+    
+    return self
+  
+  def validWords(self):
+    boardLetters = set()
+    for row in self.board:
+      for letter in row:
+        boardLetters.add(letter)
 
+    for word in self.wordList:
+      for letter in word:
+        if letter not in boardLetters:
+          break
+      else:
+        self.validWordsForBoard.append(word)
+    return self
    
-
+  def findWord(self, word):
+    pass
   
     
 
@@ -28,3 +50,5 @@ class App:
 if __name__ == "__main__":
   Game = App()
   Game.fillBoard()
+  Game.validWords()
+  print(Game.validWordsForBoard)
